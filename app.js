72519a0361dba081dbaab9c48721e08eec02c5ed
smash009 <smash009@naver.com>
2021-09-26 13:50:44 +0900

    main

--- app.js
+++ app.js
@@ -6,18 +6,30 @@ const loginInput = document.querySelector("#loginForm input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
 
 function onLoginSubmit(event) {
     event.preventDefault(); //submit 기본 동작인 refresh 방지.
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    greeting.innerText = "Hello " + username;
+    greeting.innerText = `Hello ${username}`;
     console.log(username);
     greeting.classList.remove(HIDDEN_CLASSNAME);
+    localStorage.setItem(USERNAME_KEY, username);
 };
 
 loginForm.addEventListener("submit", onLoginSubmit);
 
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null) {
+    //savedUsername 값아 없을 때 loginForm 을 보여줌.
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+} else {
+    //있으면 greeting 보여 줌.
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+};
 
 
 // function handleLoginBtnClick() {
