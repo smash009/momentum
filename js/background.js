62268b209ad7ba3dda927c7105962836b983ba29
smash009 <smash009@naver.com>
2021-09-28 05:27:47 +0900

    main

---
+++ js/background.js
@@ -0,0 +1,10 @@
+const images = ["0.jpeg", "1.jpeg", "2.jpeg"];
+const choshenImages = images[Math.floor(Math.random() * images.length)];
+const bgImage = document.createElement("img");
+
+bgImage.src= `img/${choshenImages}`;
+bgImage.alt= `${choshenImages}`;
+
+document.body.prepend(bgImage); // 이미지 태그를 body 맨뒤(append)로 배치, 맨 앞으로 배치(prepend)
+
+// 외부 랜덤 이미지로 대체
