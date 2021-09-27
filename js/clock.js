df191dadf532043a6d8b9d8f41c7d89c9382e1fd
smash009 <smash009@naver.com>
2021-09-27 22:19:54 +0900

    main

--- js/clock.js
+++ js/clock.js
@@ -1,2 +1,11 @@
 const clock = document.querySelector("h2#clock");
 
+function getClock() {
+    const date = new Date();
+    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+}
+
+
+getClock();
+setInterval(getClock, 1000);
+//setTimeout(sayHello, 5000);
