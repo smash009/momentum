a4c394a7b008b2319dcc6c119c6439082cd6450b
smash009 <smash009@naver.com>
2021-09-27 21:01:45 +0900

    main

--- app.js
+++ app.js
@@ -14,10 +14,11 @@ function onLoginSubmit(event) {
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
     localStorage.setItem(USERNAME_KEY, username);
-    paintGreetings(username);
+    paintGreetings();
 };
 
-function paintGreetings(username){
+function paintGreetings(){
+    const username = localStorage.getItem(USERNAME_KEY);
     greeting.innerText = `Hello ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 };
@@ -30,7 +31,7 @@ if(savedUsername === null) {
     loginForm.addEventListener("submit", onLoginSubmit);
 } else {
     //있으면 greeting 보여 줌.
-    paintGreetings(savedUsername);// 새로 저장한 username을 보여줌.
+    paintGreetings();// 새로 저장한 username을 보여줌.
 };
 
 
