e7448fce15a4bd1662deed3d71db023fcabc9bef
smash009 <smash009@naver.com>
2021-09-28 04:10:04 +0900

    main

--- js/clock.js
+++ js/clock.js
@@ -2,10 +2,12 @@ const clock = document.querySelector("h2#clock");
 
 function getClock() {
     const date = new Date();
-    clock.innerText = `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
+    const hours = String(date.getHours()).padStart(2, "0");
+    const minutes = String(date.getMinutes()).padStart(2, "0");
+    const seconds = String(date.getSeconds()).padStart(2, "0");
+    clock.innerText = `${hours}:${minutes}:${seconds}`;
 }
 
 
 getClock();
 setInterval(getClock, 1000);
-//setTimeout(sayHello, 5000);
