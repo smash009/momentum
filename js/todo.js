bd62182ed3608e3a478aaf6809d9c85113e79709
smash009 <smash009@naver.com>
2021-09-28 14:54:24 +0900

    main

--- js/todo.js
+++ js/todo.js
@@ -2,11 +2,20 @@ const toDoForm = document.querySelector("#toDoForm");
 const toDoInput = document.querySelector("#toDoForm input");
 const toDoList = document.querySelector("#toDoList");
 
+function paintToDo(newToDo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    li.appendChild(span);
+    span.innerText = newToDo;
+    toDoList.appendChild(li);
+}
+
 
 function handleToDoSumit(event) {
     event.preventDefault();
     const newToDo = toDoInput.value;
     toDoInput.value = "";
+    paintToDo(newToDo);
 }
 
 toDoForm.addEventListener("submit", handleToDoSumit);
