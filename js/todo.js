7e700020683d0c9f631375837cb0d60426620ddb
smash009 <smash009@naver.com>
2021-09-28 14:56:52 +0900

    main

--- js/todo.js
+++ js/todo.js
@@ -3,11 +3,11 @@ const toDoInput = document.querySelector("#toDoForm input");
 const toDoList = document.querySelector("#toDoList");
 
 function paintToDo(newToDo){
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    li.appendChild(span);
-    span.innerText = newToDo;
-    toDoList.appendChild(li);
+    const li = document.createElement("li"); // li 생성
+    const span = document.createElement("span"); // span 생성
+    li.appendChild(span); // li 안에 span 위치
+    span.innerText = newToDo; // 내용
+    toDoList.appendChild(li); // list 안에 li 위치
 }
 
 
