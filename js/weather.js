64df032c1d778202f2cedcc2147ebe682674d079
smash009 <smash009@naver.com>
2021-09-29 13:47:44 +0900

    main

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,17 @@
-const apiKey = "";
+const apiKey = "5e64673b34535dbc8e5b001eac6f5a8a";
 
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    const url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
-    console.log("You live in", lat, lng);
-    fetch(url);
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
+    
+    console.log("You live in", lat, lon);
+
+    fetch(url)
+        // .then((response) => response.json())
+        // .then((data) => {
+        // console.log(data.name, data.weather[0].main);
+    //});
     
 }
 
