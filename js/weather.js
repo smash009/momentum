012417e6053f482b6349a7967055eaa763e8d3d9
smash009 <smash009@naver.com>
2021-09-29 13:04:17 +0900

    main

--- js/weather.js
+++ js/weather.js
@@ -1,7 +1,12 @@
+const apiKey = "5e64673b34535dbc8e5b001eac6f5a8a";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
     const lng = position.coords.longitude;
+    const url = `api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}`;
     console.log("You live in", lat, lng);
+    fetch(url);
+    
 }
 
 function onGeoError() {
