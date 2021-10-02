a41edcf6287088dbee42046f68cf6d9df5f07ae1
smash009 <smash009@naver.com>
2021-10-03 01:55:54 +0900

    main

--- js/clock.js
+++ js/clock.js
@@ -1,11 +1,13 @@
 const clock = document.querySelector("h2#clock > span");
+const clock2 = document.querySelector("h2#clock > span#sec");
 
 function getClock() {
     const date = new Date();
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
-    clock.innerText = `${hours}:${minutes}:${seconds}`;
+    clock.innerText = `${hours}:${minutes}`;
+    clock2.innerText = `${seconds}`;
 }
 
 
--- js/textColor.js
+++ js/textColor.js
@@ -1,12 +1,12 @@
-const aaa = document.querySelector("h2#clock > span");
+const sec = document.querySelector("h2#clock > span#sec");
 const colors = ["#fff", "#ff0000", "#ff5e00", "#ffbb00", "#ffe400", "#abf200", "#1ddb16", "#00d8ff", "#0054ff", "#0100ff", "#5f00ff", "#ff00dd", "#ff007f"];
 
 function colorControl() {
     const randomColor1 = colors[Math.floor(Math.random() * colors.length)];
     const randomColor2 = colors[Math.floor(Math.random() * colors.length)];
-    aaa.style = `background:linear-gradient(to left, ${randomColor1}, ${randomColor2}); -webkit-background-clip: text; -webkit-text-fill-color: transparent;`;
+    sec.style = `background:linear-gradient(to left, ${randomColor1}, ${randomColor2}); -webkit-background-clip: text; -webkit-text-fill-color: transparent;`;
 };
 
-console.log(aaa);
+console.log(sec);
 colorControl();
 setInterval(colorControl, 1000);
