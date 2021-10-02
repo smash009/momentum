0fd7f7169cdaa1c5ab2330215d65b2f60557edbb
smash009 <smash009@naver.com>
2021-10-03 02:05:26 +0900

    main

--- js/clock.js
+++ js/clock.js
@@ -1,5 +1,5 @@
 const clock = document.querySelector("h2#clock > span");
-const clock2 = document.querySelector("h2#clock > span#sec");
+const clockSec = document.querySelector("h2#clock > span#sec");
 
 function getClock() {
     const date = new Date();
@@ -7,7 +7,7 @@ function getClock() {
     const minutes = String(date.getMinutes()).padStart(2, "0");
     const seconds = String(date.getSeconds()).padStart(2, "0");
     clock.innerText = `${hours}:${minutes}`;
-    clock2.innerText = `${seconds}`;
+    clockSec.innerText = `${seconds}`;
 }
 
 
