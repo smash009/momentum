f8088ff232a5fe27f37b070831ef31810b57a879
smash009 <smash009@naver.com>
2022-08-01 16:42:47 +0900

    Update day.js

--- js/day.js
+++ js/day.js
@@ -3,7 +3,7 @@ const dayH2 = document.querySelector("h2#day");
 function getDay() {
     const date = new Date();
     const year = date.getFullYear();
-    const month = date.getMonth();
+    const month = (date.getMonth()+1);
     const day = date.getDate();
     dayH2.innerText = `${year}년 ${month}월 ${day}일`;
 }
